ada482cbde4a4a14da697aef0a50277bd48ed592
vickyperezbustos <vickyperezbustos@gmail.com>
2015-09-07 00:54:09 -0300

    lo hicee! wiii

--- multiplyMatrix/tp0.c
+++ multiplyMatrix/tp0.c
@@ -1,9 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main(int argc, char *argv[])
+{
+	 char *buffer = NULL;
+	 int read;
+	 unsigned int len;
+	 char s;
+	 int fil ,col, i, j,k, fil2, col2, bytes_now;
+	 int bytes_consumed = 0;
+	 read = getline(&buffer, &len, stdin);
+
+	 while(-1 != read){
+		  bytes_consumed = 0;
+		  sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil,&s,&col, & bytes_now);
+		  //sscanf( string + bytes_consumed, "%d%n", arr , & bytes_now )
+		  bytes_consumed += bytes_now;
+		  printf("%d%c%d", fil,s,col);
+		  double matriz1[fil][col];
+		  for(i=0; i<fil;i++){
+			  for(j=0;j<col;j++){
+				  sscanf(buffer+ bytes_consumed, "%lf%n", &matriz1[i][j], & bytes_now);
+				  bytes_consumed += bytes_now;
+				  printf("%lf", matriz1[i][j]);
+			  }
+		  }
+		  read = getline(&buffer, &len, stdin);
+		  bytes_consumed = 0;
+		  sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil2,&s,&col2, & bytes_now);
+		  bytes_consumed += bytes_now;
+		  printf("%d%c%d", fil2,s,col2);
+		 	  double matriz2[fil2][col2];
+		 	  for(i=0; i<fil2;i++){
+		 		  for(j=0;j<col2;j++){
+		 			  sscanf(buffer+ bytes_consumed, "%lf%n", &matriz2[i][j], & bytes_now);
+		 			  bytes_consumed += bytes_now;
+		 			  printf("%lf", matriz2[i][j]);
+		 		  }
+		 	  }
+		 if (col== fil2){
+
+			 double sum;
+			 double out[fil][col2];
+			 	 for(i=0;i<fil;i++){ // fila de matriz 1
+			 		 for(j=0;j<col2;j++){  // columna de segunda matriz
+			 			 sum=0;
+			 			 for(k=0;k<col;k++)
+			 				 sum=sum+matriz1[i][k]*matriz2[k][j];
+			 				 out[i][j]=sum;
+			 		 }
+			 	 }
+		 	printf("\n Matriz resultado \n");
+		 	bytes_consumed = 0;
+		 	sprintf(buffer+ bytes_consumed,"%dx%d%n", fil,col2, & bytes_now);
+		 	bytes_consumed += bytes_now;
+		 	for(i=0;i<fil;i++){
+		 		for(j=0;j<col2;j++){
+		 			sprintf(buffer+ bytes_consumed," %lf%n",out[i][j], & bytes_now);
+		 			bytes_consumed += bytes_now;
+		 		}
+		 	}
+		 	puts(buffer);
+		 }
+		 else{
+			 printf("matriz incorrecta para la multiplicacion");
+		 	 return 0; // escribir error en archivo
+		 }
+		 read = getline(&buffer, &len, stdin);
+	 }
+
+	 printf("\n Size read: %d\n Len: %d\n", read, len);
+	 free(buffer);
+	 return 0;
+}
+
+
+
 /*
- * tp0.c
+
+
+* tp0.c
  *
  *  Created on: 31/08/2015
  *      Author: sami
- */
+
 
 #include "tp0.h"
 extern int errno ;
@@ -43,9 +123,7 @@ int checkArguments(int cantidadArgumentos, char* argumentos[]) {
 
 
 int multiplicarMatrices(double v1[],int f1, int c1, double v2[], int f2, int c2, double m1[f1][c1], double m2[f2][c2] ) {
-	/*
-	 * Datos que vienen en la línea stdin
-	 */
+
 	int i,j, k = 0;
 	double out[f1][c2];
 	//Completo la matriz 1
@@ -192,7 +270,7 @@ int main(int argc, char* argv[]){
 		//return 1;
 	}
 
-}
+}*/
 
 
 
