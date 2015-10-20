9e901f2d0c7d9d0d8967bb7cbd8d2b53e1f3d96c
vickyperezbustos <vickyperezbustos@gmail.com>
2015-10-19 23:29:49 -0300

    subo ultima version

--- multiplyMatrix/tp0.c
+++ multiplyMatrix/tp0.c
@@ -1,3 +1,4 @@
+
 #include "tp0.h"
 
 //extern int errno ;
@@ -56,7 +57,7 @@ int checkArguments(int cantidadArgumentos, char* argumentos[]) {
 // 		}
 // 	}
 // 	return 0;
-	
+//
 // }
 
 
@@ -86,7 +87,7 @@ int validoLinea(char *linea){
 		desp++;
 		col = *(linea+desp);
 	}
-	
+
 	int cantNums = 0;
 	char car =*(linea+desp);
 	if(car == ' '){
@@ -95,7 +96,7 @@ int validoLinea(char *linea){
 	}else{
 		fprintf(stderr, "Bad space \n" );
 	}
-	
+
 	while(car!= 0 && car != EOF){
 		bool point = false;
 		while((car != ' ' && car >= '0' && car <= '9') || (car == '.')){
@@ -112,7 +113,7 @@ int validoLinea(char *linea){
 
 		}
 		desp++;
-		car = *(linea+desp);	
+		car = *(linea+desp);
 		cantNums++;
 
 	}
@@ -127,119 +128,194 @@ int validoLinea(char *linea){
 		return 1;
 	}else{
 
-		char *buffer= NULL;
-
-		int read = 0;
-		size_t len =0;
+		//char *buffer= NULL;
+//
+		//int read = 0;
+		//size_t len =0;
 		char s;
 		int fil=0 ,col=0, i =0, fil2=0, col2=0, bytes_now=0;
 		int bytes_consumed = 0;
-		read = getline(&buffer, &len, stdin);
 
-		if(read == -1){
-			//No recibio stream de entrada
-			fprintf(stderr,"No stdin \n");
-			free(buffer);
-			exit(1);
-		}
-		while(-1 != read){
-			int cantNumsMatriz = validoLinea(buffer);
-			if(cantNumsMatriz == -1){
-				fprintf(stderr,"Linea incorrecta. %s \n", buffer);				
-				free(buffer);
-				exit(1);
-			}
-			double matriz1[cantNumsMatriz];
-			bytes_consumed = 0;
-			sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil,&s,&col, & bytes_now);
-			bytes_consumed += bytes_now;
-			if(cantNumsMatriz!= (fil*col)){
-				fprintf(stderr,"Matriz incorrecta. %s \n", buffer);
-				free(buffer);
-				exit(1);
-			}
-			for(i = 0 ; i < cantNumsMatriz; i++){
-				sscanf(buffer+ bytes_consumed, "%lf%n", &matriz1[i], & bytes_now);
-				bytes_consumed += bytes_now;
-			}
-			// for(i=0; i<fil;i++){
-			// 	for(j=0;j<col;j++){
-			// 		sscanf(buffer+ bytes_consumed, "%lf%n", &matriz1[i][j], & bytes_now);
-			// 		bytes_consumed += bytes_now;
-			// 	}
-			// }
-			memset (buffer,0,SIZE_MAT);
-			read = getline(&buffer, &len, stdin);
-			cantNumsMatriz = validoLinea(buffer);
-			double matriz2[cantNumsMatriz];
-			bytes_consumed = 0;
-			sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil2,&s,&col2, & bytes_now);
-			bytes_consumed += bytes_now;
-			if(cantNumsMatriz!= (fil2*col2)){
-				fprintf(stderr,"Matriz incorrecta. %s", buffer);
-				free(buffer);
-				exit(1);
-			}
-			for(i = 0 ; i < cantNumsMatriz; i++){
-				sscanf(buffer+ bytes_consumed, "%lf%n", &matriz2[i], & bytes_now);
-				bytes_consumed += bytes_now;
-			}
-			// for(i=0; i<fil2;i++){
-			// 	for(j=0;j<col2;j++){
-			// 		sscanf(buffer+ bytes_consumed, "%lf%n", &matriz2[i][j], & bytes_now);
-			// 		bytes_consumed += bytes_now;
-			// 	}
-			// }
-			if (col== fil2){
-				double out[fil*col2];
-				int status = -1;
-				status = multiplicarMatrices(fil, col, fil2, col2, &matriz1, &matriz2, &out);
-				if(status == 0){
-					// printf("Multiplicacion ok \n");
-					// for(int h = 0; h < fil*col2; h++){
-					// 	printf("%lf ", out[h] );
-					// }
-				}else{
-					fprintf(stderr,"Multiplicacion incorrecta. %s", buffer);
-				}
+		char buffer[BUFSIZ];
 
-				// double sum;
-				// double out[fil][col2];
-				// for(i=0;i<fil;i++){ // fila de matriz 1
-				// 	for(j=0;j<col2;j++){  // columna de segunda matriz
-				// 		sum=0;
-				// 		for(k=0;k<col;k++)
-				// 			sum=sum+matriz1[i][k]*matriz2[k][j];
-				// 			out[i][j]=sum;
-				// 	}
-				// }
-				bytes_consumed = 0;
+		while (fgets(buffer, sizeof(buffer), stdin) != 0)
+		{
 
-				memset (buffer,0,SIZE_MAT);
-				sprintf(buffer+ bytes_consumed,"%dx%d%n\n", fil,col2, & bytes_now);
-				bytes_consumed += bytes_now;
-				for(i=0;i<fil*col2;i++){				
-					sprintf(buffer+ bytes_consumed," %lf%n",out[i], & bytes_now);
-					bytes_consumed += bytes_now;
-				}
-				// for(i=0;i<fil;i++){
-				// 	for(j=0;j<col2;j++){
-				// 		sprintf(buffer+ bytes_consumed," %lf%n",out[i][j], & bytes_now);
-				// 		bytes_consumed += bytes_now;
-				// 	}
-				// }
-				puts(buffer);
-			}else{
-				fprintf(stderr,"Matrices incorrectas para la multiplicacion. \n"); 
-				free(buffer);
-				exit(1); 
-			}
-			memset (buffer,0,SIZE_MAT);
-			read = getline(&buffer, &len, stdin);
+		    int cantNumsMatriz = validoLinea(buffer);
+		    if(cantNumsMatriz == -1){
+		    	fprintf(stderr,"Linea incorrecta. %s \n", buffer);
+		    	free(buffer);
+		    	exit(1);
+		   }
+
+		    double matriz1[cantNumsMatriz];
+		    bytes_consumed = 0;
+		    sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil,&s,&col, & bytes_now);
+		    bytes_consumed += bytes_now;
+		    if(cantNumsMatriz!= (fil*col)){
+		    	fprintf(stderr,"Matriz incorrecta. %s \n", buffer);
+		    		free(buffer);
+		    		exit(1);
+		   }
+		   	for(i = 0 ; i < cantNumsMatriz; i++){
+		    	sscanf(buffer+ bytes_consumed, "%lf%n", &matriz1[i], & bytes_now);
+		    	bytes_consumed += bytes_now;
+		    }
+
+		    memset(buffer,0,SIZE_MAT);
+		    fgets(buffer, sizeof(buffer), stdin);
+		    cantNumsMatriz = validoLinea(buffer);
+		    double matriz2[cantNumsMatriz];
+		    bytes_consumed = 0;
+		    sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil2,&s,&col2, & bytes_now);
+		    bytes_consumed += bytes_now;
+		    if(cantNumsMatriz!= (fil2*col2)){
+		    	fprintf(stderr,"Matriz incorrecta. %s", buffer);
+		    	free(buffer);
+		    	exit(1);
+		    }
+		    for(i = 0 ; i < cantNumsMatriz; i++){
+		    	sscanf(buffer+ bytes_consumed, "%lf%n", &matriz2[i], & bytes_now);
+		    	bytes_consumed += bytes_now;
+		    }
+
+		    if (col== fil2){
+		    	double out[fil*col2];
+		    	int status = -1;
+		    	status = multiplicarMatrices(fil, col, fil2, col2, &matriz1, &matriz2, &out);
+		    	if(status == 0){
+
+		    	}else{
+		    		fprintf(stderr,"Multiplicacion incorrecta. %s", buffer);
+		    	}
+
+		    	bytes_consumed = 0;
+		    	memset (buffer,0,SIZE_MAT);
+		    	sprintf(buffer+ bytes_consumed,"%dx%d%n\n", fil,col2, & bytes_now);
+		    	bytes_consumed += bytes_now;
+		    	for(i=0;i<fil*col2;i++){
+		    		sprintf(buffer+ bytes_consumed," %lf%n",out[i], & bytes_now);
+		    		bytes_consumed += bytes_now;
+		    	}
+
+		    	puts(buffer);
+		    }else{
+		    	fprintf(stderr,"Matrices incorrectas para la multiplicacion. \n");
+		    	free(buffer);
+		    	exit(1);
+		    }
+		    memset (buffer,0,SIZE_MAT);
 
 		}
 
-		free(buffer);
-		return 0;
+//
+//		read = getline(&buffer, &len, stdin);
+//
+//		if(read == -1){
+//			//No recibio stream de entrada
+//			fprintf(stderr,"No stdin \n");
+//			free(buffer);
+//			exit(1);
+//		}
+//		while(-1 != read){
+//			int cantNumsMatriz = validoLinea(buffer);
+//			if(cantNumsMatriz == -1){
+//				fprintf(stderr,"Linea incorrecta. %s \n", buffer);
+//				free(buffer);
+//				exit(1);
+//			}
+//			double matriz1[cantNumsMatriz];
+//			bytes_consumed = 0;
+//			sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil,&s,&col, & bytes_now);
+//			bytes_consumed += bytes_now;
+//			if(cantNumsMatriz!= (fil*col)){
+//				fprintf(stderr,"Matriz incorrecta. %s \n", buffer);
+//				free(buffer);
+//				exit(1);
+//			}
+//			for(i = 0 ; i < cantNumsMatriz; i++){
+//				sscanf(buffer+ bytes_consumed, "%lf%n", &matriz1[i], & bytes_now);
+//				bytes_consumed += bytes_now;
+//			}
+//			// for(i=0; i<fil;i++){
+//			// 	for(j=0;j<col;j++){
+//			// 		sscanf(buffer+ bytes_consumed, "%lf%n", &matriz1[i][j], & bytes_now);
+//			// 		bytes_consumed += bytes_now;
+//			// 	}
+//			// }
+//			memset(buffer,0,SIZE_MAT);
+//			read = getline(&buffer, &len, stdin);
+//			cantNumsMatriz = validoLinea(buffer);
+//			double matriz2[cantNumsMatriz];
+//			bytes_consumed = 0;
+//			sscanf(buffer+ bytes_consumed, "%d%c%d%n", &fil2,&s,&col2, & bytes_now);
+//			bytes_consumed += bytes_now;
+//			if(cantNumsMatriz!= (fil2*col2)){
+//				fprintf(stderr,"Matriz incorrecta. %s", buffer);
+//				free(buffer);
+//				exit(1);
+//			}
+//			for(i = 0 ; i < cantNumsMatriz; i++){
+//				sscanf(buffer+ bytes_consumed, "%lf%n", &matriz2[i], & bytes_now);
+//				bytes_consumed += bytes_now;
+//			}
+//			// for(i=0; i<fil2;i++){
+//			// 	for(j=0;j<col2;j++){
+//			// 		sscanf(buffer+ bytes_consumed, "%lf%n", &matriz2[i][j], & bytes_now);
+//			// 		bytes_consumed += bytes_now;
+//			// 	}
+//			// }
+//			if (col== fil2){
+//				double out[fil*col2];
+//				int status = -1;
+//				//status = multiplicarMatrices(fil, col, fil2, col2, &matriz1, &matriz2, &out);
+//				if(status == 0){
+//					// printf("Multiplicacion ok \n");
+//					// for(int h = 0; h < fil*col2; h++){
+//					// 	printf("%lf ", out[h] );
+//					// }
+//				}else{
+//					fprintf(stderr,"Multiplicacion incorrecta. %s", buffer);
+//				}
+//
+//				// double sum;
+//				// double out[fil][col2];
+//				// for(i=0;i<fil;i++){ // fila de matriz 1
+//				// 	for(j=0;j<col2;j++){  // columna de segunda matriz
+//				// 		sum=0;
+//				// 		for(k=0;k<col;k++)
+//				// 			sum=sum+matriz1[i][k]*matriz2[k][j];
+//				// 			out[i][j]=sum;
+//				// 	}
+//				// }
+//				bytes_consumed = 0;
+//
+//				memset (buffer,0,SIZE_MAT);
+//				sprintf(buffer+ bytes_consumed,"%dx%d%n\n", fil,col2, & bytes_now);
+//				bytes_consumed += bytes_now;
+//				for(i=0;i<fil*col2;i++){
+//					sprintf(buffer+ bytes_consumed," %lf%n",out[i], & bytes_now);
+//					bytes_consumed += bytes_now;
+//				}
+//				// for(i=0;i<fil;i++){
+//				// 	for(j=0;j<col2;j++){
+//				// 		sprintf(buffer+ bytes_consumed," %lf%n",out[i][j], & bytes_now);
+//				// 		bytes_consumed += bytes_now;
+//				// 	}
+//				// }
+//				puts(buffer);
+//			}else{
+//				fprintf(stderr,"Matrices incorrectas para la multiplicacion. \n");
+//				free(buffer);
+//				exit(1);
+//			}
+//			memset (buffer,0,SIZE_MAT);
+//			read = getline(&buffer, &len, stdin);
+//
+//		}
+//
+//		free(buffer);
+//		return 0;
 	}
+	return 0;
 }
